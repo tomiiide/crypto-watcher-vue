30ef9b443f79104f8d1c6deb921539276641c6cc
tomiiide <ayotomiiide@gmail.com>
2017-12-16 16:47:20 +0100

    Initial commit

---
+++ src/components/Crypto-item.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="crypto-item">
+    <img src="" alt="">
+    <span class="name"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CryptoItem',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ .crypto-item{
+
+ }
+</style>
---
+++ src/components/CryptoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="crypto">
+    <div class="header">CryptoWatcher</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CryptoList',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .header{
+    
+  }
+</style>
